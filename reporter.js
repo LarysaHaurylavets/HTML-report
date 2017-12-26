17ba6c86c288e82112d2e10fd492d2ba94d4b6c3
Larysa Haurylavets <Larysa_Haurylavets@epam.com>
2017-12-26 10:16:39 +0300

    Update reporter.js

--- reporter.js
+++ reporter.js
@@ -6,35 +6,35 @@ var _ = require('lodash');
 // Custom reporter
 var Reporter = function (options) {
 
-//for html document
-var indexHtmlFile = './node_modules/html-reporter/index.html';
-var specAll = 0;
-var specFailed = 0;
-var specPassed = 0;
+  //for html document
+  var indexHtmlFile = './node_modules/html-reporter/index.html';
+  var specAll = 0;
+  var specFailed = 0;
+  var specPassed = 0;
 
-var _defaultOutputFile = path.resolve(process.cwd(), './_test-output', 'protractor-results.html');
-options.outputFile = options.outputFile || _defaultOutputFile;
+  var _defaultOutputFile = path.resolve(process.cwd(), './_test-output', 'protractor-results.html');
+  options.outputFile = options.outputFile || _defaultOutputFile;
 
-initOutputFile(options.outputFile);
-options.appDir = options.appDir || './';
-var _root = { appDir: options.appDir, suites: [] };
-log('AppDir: ' + options.appDir, +1);
-var _currentSuite;
+  initOutputFile(options.outputFile);
+  options.appDir = options.appDir || './';
+  var _root = { appDir: options.appDir, suites: [] };
+  log('AppDir: ' + options.appDir, +1);
+  var _currentSuite;
 
-this.suiteStarted = function (suite) {
-_currentSuite = { description: suite.description, status: null, specs: [] };
-_root.suites.push(_currentSuite);
-log('Suite: ' + suite.description, +1);
-};
+  this.suiteStarted = function (suite) {
+    _currentSuite = { description: suite.description, status: null, specs: [] };
+    _root.suites.push(_currentSuite);
+    log('Suite: ' + suite.description, +1);
+  };
 
-this.suiteDone = function (suite) {
-  var statuses = _currentSuite.specs.map(function (spec) {
-    return spec.status;
-  });
+  this.suiteDone = function (suite) {
+    var statuses = _currentSuite.specs.map(function (spec) {
+      return spec.status;
+    });
 
-  statuses = _.uniq(statuses);
-  var status = statuses.indexOf('failed') >= 0 ? 'failed' : statuses.join(', ');
-  _currentSuite.status = status;
+    statuses = _.uniq(statuses);
+    var status = statuses.indexOf('failed') >= 0 ? 'failed' : statuses.join(', ');
+    _currentSuite.status = status;
     log('Suite ' + _currentSuite.status + ': ' + suite.description, -1);
   };
 
@@ -45,11 +45,11 @@ this.suiteDone = function (suite) {
 
   this.specDone = function (spec) {
 
-    var screenshotDir = path.dirname(options.outputFile)+'/screens/_';
-    var screenshotName = screenshotDir+spec.description.replace(/\s+/g,"_")+'.png';
+    var screenshotDir = path.dirname(options.outputFile) + '/screens/_';
+    var screenshotName = screenshotDir + spec.description.replace(/\s+/g, "_") + '.png';
 
     //take a screenshot
-    browser.takeScreenshot().then(function(png) {
+    browser.takeScreenshot().then(function (png) {
       var stream = fs.createWriteStream(screenshotName);
       stream.write(new Buffer(png, 'base64'));
       stream.end();
@@ -97,42 +97,42 @@ this.suiteDone = function (suite) {
     }
   }
 
-//function for read index.html
-function readHtmlFile(htmlFile){
-  return fs.readFileSync(htmlFile, 'UTF-8');
-}
+  //function for read index.html
+  function readHtmlFile(htmlFile) {
+    return fs.readFileSync(htmlFile, 'UTF-8');
+  }
 
-function initOutputFile(outputFile) {
-  ensureDirectoryExistence(outputFile, '/screens');
-  var htmlFile = readHtmlFile(indexHtmlFile);
-  var header = "<div>Protractor results for: " + (new Date()).toLocaleString() + '</div>';
-  fs.writeFileSync(outputFile, htmlFile+header, 'UTF-8');
-}
+  function initOutputFile(outputFile) {
+    ensureDirectoryExistence(outputFile, '/screens');
+    var htmlFile = readHtmlFile(indexHtmlFile);
+    var header = "<div>Protractor results for: " + (new Date()).toLocaleString() + '</div>';
+    fs.writeFileSync(outputFile, htmlFile + header, 'UTF-8');
+  }
 
-// for output file output
+  // for output file output
   function formatOutput(output) {
     var indent = '  ';
     var pad = '  ';
     var results = [];
-    results.push('<p>AppDir:' + output.appDir+'</p>');
-    results.push('<h3>Total tests:'+ specAll + '</h3>');
-    results.push('<p>Passed tests:<div class="passed">'+specPassed+'</div></p>');
-    results.push('<p>Failed tests:<div class="failed">'+specFailed+'</div></p>');
-    
+    results.push('<p>AppDir:' + output.appDir + '</p>');
+    results.push('<h3>Total tests:' + specAll + '</h3>');
+    results.push('<p>Passed tests:<div class="passed">' + specPassed + '</div></p>');
+    results.push('<p>Failed tests:<div class="failed">' + specFailed + '</div></p>');
+
     output.suites.forEach(function (suite) {
-      
-      results.push('<p>' + 'Suite: ' + suite.description + ' -- ' + suite.status+'</p><hr>');
+
+      results.push('<p>' + 'Suite: ' + suite.description + ' -- ' + suite.status + '</p><hr>');
       pad += indent;
-      suite.specs.forEach(function (spec) {       
-        results.push('<p>' + spec.description+'<div class="passed">'+spec.status + '</div> </p>');
-        results.push('<img src='+spec.img+'  width="1200" height="800"/>');
+      suite.specs.forEach(function (spec) {
+        results.push('<p>' + spec.description + '<div class="passed">' + spec.status + '</div> </p>');
+        results.push('<img src=' + spec.img + '  width="1200" height="800"/>');
 
         if (spec.failedExpectations) {
           pad += indent;
-          spec.failedExpectations.forEach(function (fe) {          
-            results.push('<p><div class="failed">message: ' + fe.message+'</div></p>');
+          spec.failedExpectations.forEach(function (fe) {
+            results.push('<p><div class="failed">message: ' + fe.message + '</div></p>');
           });
-          
+
         }
       });
 
@@ -143,8 +143,8 @@ function initOutputFile(outputFile) {
   }
 
 
-// for console output
-var _pad;
+  // for console output
+  var _pad;
   function log(str, indent) {
     _pad = _pad || '';
     if (indent == -1) {
@@ -155,7 +155,7 @@ var _pad;
     if (indent == 1) {
       _pad = _pad + '  ';
     }
-   }
+  }
 
 };
 
